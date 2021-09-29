e7e6f89a5c79b478cf4a638b8074475318f0bce4
NicksPatties <nickspatties@gmail.com>
2021-09-29 13:34:48 -0600

    Add Pager composable package. Implement swipable pager in the app.

--- app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
+++ app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 import com.nickspatties.timeclock.ui.theme.TimeClockTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +25,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun TimeClockApp() {
+fun TimeClockApp(initialPage: Int = 0) {
     TimeClockTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
+            val pagerState = rememberPagerState(
+                pageCount = 3,
+                initialPage = initialPage // temporary, but just for testing
+            )
+
+            HorizontalPager(
+                state = pagerState
+            ) { page ->
+                PageSelector(page)
+            }
+
+            HorizontalPagerIndicator(
+                pagerState = pagerState
+            )
         }
+    }
+}
+
+@Composable
+fun PageSelector(pageId: Int) {
+    when(pageId) {
+        0 -> SamplePage("One")
+        1 -> SamplePage("Two")
+        2 -> SamplePage("Three")
+    }
+}
+
+@Composable
+fun SamplePage(name: String) {
+    Scaffold() {
+        Greeting(name = name)
     }
 }
 
@@ -36,6 +72,18 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun AppPreview() {
+fun FirstPagePreview() {
     TimeClockApp()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondPagePreview() {
+    TimeClockApp(1)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdPagePreview() {
+    TimeClockApp(2)
 }
