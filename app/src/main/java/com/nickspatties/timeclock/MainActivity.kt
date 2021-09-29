85810c5601bcc10fa3cafca0894e02c1c2a2dfb3
NicksPatties <nickspatties@gmail.com>
2021-09-28 20:11:29 -0600

    Miscellaneous changes. Move TimeClockTheme code to its own composable function.

--- app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
+++ app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
@@ -14,12 +14,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimeClockTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            TimeClockApp()
+        }
+    }
+}
+
+@Composable
+fun TimeClockApp() {
+    TimeClockTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Android")
         }
     }
 }
@@ -31,8 +36,6 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TimeClockTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    TimeClockApp()
 }
