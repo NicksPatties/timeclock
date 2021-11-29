c9658b8cf6add0ca5438b7f56faaccdc1ef3bc2d
NicksPatties <nickspatties@gmail.com>
2021-11-29 13:51:18 -0700

    Update chronometer to have flexible delays.

--- app/src/main/java/com/nickspatties/timeclock/util/Chronometer.kt
+++ app/src/main/java/com/nickspatties/timeclock/util/Chronometer.kt
@@ -12,22 +12,45 @@ class Chronometer {
 
     // TODO: Should I allow this class to execute on a different thread?
     private var mHandler: Handler = Handler(Looper.getMainLooper())
-    private var mBase: Long = 0
     private var mRunning: Boolean = false
     private var mOnChronometerTickListener: () -> Unit = {}
+    private val mDefaultDelay = 1000L
+    private var mDelay = mDefaultDelay
+    private var mTickFrequency = mDefaultDelay
 
-    // TODO: create an interface for different listeners, then when you're creating the ViewModel
-    // for this app, have the ViewModel implement the members of that interface
+    /* TODO create an interface for different listeners, then when you're creating the ViewModel
+     *  for this app, have the ViewModel implement the members of that interface. Stuff like:
+     *    onChronometerStart
+     *    onChronometerEnd
+     */
+    /**
+     * Assigns the function that executes with each tick of the chronometer.
+     *
+     * @param function the code to execute
+     */
     fun setOnChronometerTickListener(function: () -> Unit) {
         mOnChronometerTickListener = function
     }
 
-    fun start() {
-        mBase = SystemClock.elapsedRealtime()
+    /**
+     * Starts the chronometer, which executes the onChronometerTickListener after a given
+     * delay, and repeats with a given frequency
+     *
+     * @param customDelay the amount of time in millis that the onChronometerTickListener should
+     * execute for the first time
+     * @param customTickFrequency the amount of time in millis between each subsequent execution
+     * of the onChronometerTickListener
+     */
+    fun start(customDelay: Long = mDefaultDelay, customTickFrequency: Long = mDefaultDelay) {
         mRunning = true
-        mHandler.postDelayed(mTickRunnable, 1000)
+        mDelay = customDelay
+        mTickFrequency = customTickFrequency
+        mHandler.postDelayed(mTickRunnable, mDelay)
     }
 
+    /**
+     * Stops the onChronometerTickListener from being executed anymore
+     */
     fun stop() {
         mRunning = false
         mHandler.removeCallbacks(mTickRunnable)
@@ -36,14 +59,9 @@ class Chronometer {
     private val mTickRunnable: Runnable = object : Runnable {
         override fun run() {
             if (mRunning) {
-                dispatchChronometerTick()
-                mHandler.postDelayed(this, 1000)
+                mOnChronometerTickListener()
+                mHandler.postDelayed(this, mTickFrequency)
             }
         }
     }
-
-    fun dispatchChronometerTick() {
-        mOnChronometerTickListener()
-    }
-
 }
