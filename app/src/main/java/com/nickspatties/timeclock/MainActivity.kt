de48fd52280533feb0cf48330c34ea004f7113e4
NicksPatties <nickspatties@gmail.com>
2022-04-19 13:04:55 -0700

    Remove pager functionality

--- app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
+++ app/src/main/java/com/nickspatties/timeclock/MainActivity.kt
@@ -7,13 +7,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layout
 import androidx.lifecycle.ViewModelProvider
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.HorizontalPagerIndicator
-import com.google.accompanist.pager.rememberPagerState
 import com.nickspatties.timeclock.data.TimeClockEventDatabase
 import com.nickspatties.timeclock.ui.TimeClockViewModel
 import com.nickspatties.timeclock.ui.TimeClockViewModelFactory
@@ -44,38 +38,12 @@ class MainActivity : ComponentActivity() {
  * initialPage - index of the initial page to test. 0 for clock page,
  * 1 for ListPage, 2 for AnalysisPage
  */
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun TimeClockApp(initialPage: Int = 0, viewModel: TimeClockViewModel) {
     TimeClockTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-            val pagerState = rememberPagerState(
-                pageCount = 3,
-                initialPage = initialPage // temporary, but just for testing
-            )
-
-            HorizontalPager(
-                state = pagerState
-            ) { page ->
-                PageSelector(page, viewModel)
-            }
-
-            HorizontalPagerIndicator(
-                pagerState = pagerState,
-                modifier = Modifier.layout { measurable, constraints ->
-                    val placeable = measurable.measure(constraints)
-                    layout(placeable.width, placeable.height) {
-                        // place x at half of the screen size minus half the width of
-                        // the horizontal pager indicator
-                        val xPos = (constraints.maxWidth / 2) - (placeable.width / 2)
-
-                        // place y at the bottom of the screen, minus some padding
-                        val yPos = constraints.maxHeight - 100
-                        placeable.placeRelative(xPos, yPos)
-                    }
-                }
-            )
+            PageSelector(initialPage, viewModel)
         }
     }
 }
