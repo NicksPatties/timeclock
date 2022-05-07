ef2a3cd9c757aaa9e298437a9b265eadaca82614
NicksPatties <nickspatties@gmail.com>
2022-05-06 18:35:26 -0600

    Create sortByNamesAndTotalMillis event transformation, added test

--- app/src/main/java/com/nickspatties/timeclock/util/EventTransforms.kt
+++ app/src/main/java/com/nickspatties/timeclock/util/EventTransforms.kt
@@ -13,3 +13,19 @@ fun getAutofillValues(events: List<TimeClockEvent>): Set<String> {
         it.name
     }.toSet()
 }
+
+fun sortByNamesAndTotalMillis(events: List<TimeClockEvent>): List<Pair<String, Long>> {
+    val eventNameAndDurationMap : MutableMap<String, Long> = mutableMapOf()
+    for (event in events) {
+        val name = event.name
+        val duration = event.endTime - event.startTime
+        if (eventNameAndDurationMap[name] == null) {
+            eventNameAndDurationMap[name] = duration
+        } else {
+            val currentDuration = eventNameAndDurationMap[name]!!
+            eventNameAndDurationMap[name] = currentDuration + duration
+        }
+    }
+    // return a list of events sorted by the durations
+    return eventNameAndDurationMap.toList().sortedByDescending { it.second }
+}
--- app/src/test/java/com/nickspatties/timeclock/util/EventTransformsTest.kt
+++ app/src/test/java/com/nickspatties/timeclock/util/EventTransformsTest.kt
@@ -1,4 +1,43 @@
 package com.nickspatties.timeclock.util
 
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
 class EventTransformsTest {
+
+    @Test
+    fun sortByNamesAndTotalMillis_returnsExpectedList() {
+        val programmingDuration = convertHoursMinutesSecondsToMillis(1)
+        val bikingDuration = convertHoursMinutesSecondsToMillis(1)
+        val readingDuration = convertHoursMinutesSecondsToMillis(0, 30)
+
+        val mockEvents = createMockTimeClockEventList(
+            eventCount = 3,
+            eventNames = listOf(
+                "Programming",
+                "Biking",
+                "Reading"
+            ),
+            eventDurations = listOf(
+                programmingDuration,
+                bikingDuration,
+                readingDuration
+            )
+        )
+
+        val analysisPageList = sortByNamesAndTotalMillis(mockEvents)
+        val expectedPairs = listOf(
+            Pair("Biking", bikingDuration),
+            Pair("Programming", programmingDuration),
+            Pair("Reading", readingDuration)
+        )
+
+        for ((i, item) in analysisPageList.withIndex()) {
+            val expectedPair = expectedPairs[i]
+            // assert the names are the same
+            assertThat(item.first).isEqualTo(expectedPair.first)
+            // assert the duration in millis is the same
+            assertThat(item.second).isEqualTo(expectedPair.second)
+        }
+    }
 }
