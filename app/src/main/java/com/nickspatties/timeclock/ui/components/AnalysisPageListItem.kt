a5a94f06b7c4b66d243e2b1efb499b762fb14d30
NicksPatties <nickspatties@gmail.com>
2022-05-09 09:45:42 -0600

    Add constraint-layout to project. Use constraint layout to fix time overflow problem in AnalysisPageListItem.

--- app/src/main/java/com/nickspatties/timeclock/ui/components/AnalysisPageListItem.kt
+++ app/src/main/java/com/nickspatties/timeclock/ui/components/AnalysisPageListItem.kt
@@ -9,30 +9,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.nickspatties.timeclock.util.convertHoursMinutesSecondsToMillis
 import com.nickspatties.timeclock.util.decorateMillisWithDecimalHours
 import com.nickspatties.timeclock.util.generateColorFromString
 
-// TODO: may benefit from constraint layout
 @Composable
 fun AnalysisPageListItemClosedContent(
     taskName: String,
     totalHours: String
 ) {
-    Row(
-        modifier = Modifier.fillMaxHeight(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+    ConstraintLayout(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+
     ) {
+        val (name, time) = createRefs()
         Text(
-            modifier = Modifier.fillMaxWidth(0.75f),
+            modifier = Modifier.constrainAs(name) {
+                start.linkTo(parent.start)
+                end.linkTo(time.start)
+                centerVerticallyTo(parent)
+                width = Dimension.fillToConstraints
+            },
             text = taskName,
             style = MaterialTheme.typography.body1,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
         Text(
-            modifier = Modifier.padding(start = 8.dp),
+            modifier = Modifier.constrainAs(time) {
+                start.linkTo(name.end)
+                end.linkTo(parent.end)
+                centerVerticallyTo(parent)
+            },
             text = totalHours,
             maxLines = 1,
             style = MaterialTheme.typography.h6,
@@ -43,7 +53,7 @@ fun AnalysisPageListItemClosedContent(
 @Preview(showBackground = true)
 @Composable
 fun ClosedAnalysisPageListItem() {
-    val titleName = "programming"
+    val titleName = "Mega hyper long ass name that takes"
     val accentColor = generateColorFromString(titleName)
     val totalTime = decorateMillisWithDecimalHours(
         convertHoursMinutesSecondsToMillis(999)
