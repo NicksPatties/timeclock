40f587284d802eca2f7cd9ebe59d4730407274e3
NicksPatties <nickspatties@gmail.com>
2022-05-10 10:26:32 -0500

    Animate pie graph when viewing metrics page.

--- app/src/main/java/com/nickspatties/timeclock/ui/pages/AnalysisPage.kt
+++ app/src/main/java/com/nickspatties/timeclock/ui/pages/AnalysisPage.kt
@@ -1,15 +1,17 @@
 package com.nickspatties.timeclock.ui.pages
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nickspatties.timeclock.ui.components.AnalysisPageListItemContent
@@ -89,13 +91,50 @@ fun PieChart(
     analysisPageRows.forEach {
         totalTime += it.second
     }
+    val stroke = with(LocalDensity.current) { Stroke(15.dp.toPx()) }
 
-    // todo: define animations
-    var startAngle = -90f
-    var pieSegment = 360f
+    // instantly change the state from START to END, applying the transitions
+    // in the animations below
+    val currentState = remember {
+        MutableTransitionState(AnimatedCircleProgress.START)
+            .apply { targetState = AnimatedCircleProgress.END }
+    }
+    val transition = updateTransition(currentState, label = "")
+    val animationDelay = 400
+    val animationDuration = 500
+    val customTween : TweenSpec<Float> = tween(
+        delayMillis = animationDelay,
+        durationMillis = animationDuration,
+        easing = FastOutSlowInEasing
+    )
+
+    // proportion of the pie that is filled in
+    val pieSegment by transition.animateFloat(
+        transitionSpec = { customTween },
+        label = ""
+    ) { progress ->
+        if (progress == AnimatedCircleProgress.START) {
+            0f
+        } else {
+            360f
+        }
+    }
+    // where the pie starts being drawn
+    val shift by transition.animateFloat(
+        transitionSpec = { customTween },
+        label = ""
+    ) { progress ->
+        if (progress == AnimatedCircleProgress.START) {
+            -45f
+        } else {
+            0f
+        }
+    }
 
     Canvas(
-        modifier = Modifier.fillMaxHeight().fillMaxWidth()
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
     ) {
         // define bounds of circle
         val innerRadius = (size.minDimension) / 2
@@ -105,24 +144,27 @@ fun PieChart(
             halfSize.height - innerRadius
         )
         val size = Size(innerRadius * 2, innerRadius * 2)
-
+        var startAngle = shift - 90f
         analysisPageRows.forEach { row ->
             val color = generateColorFromString(row.first)
             val percentage = row.second / totalTime.toFloat() * pieSegment
             val alpha = if (currId == row.third || currId == -1L) 1.0f else 0.5f
             drawArc(
+                style = stroke,
                 color = color,
                 startAngle = startAngle,
                 sweepAngle = percentage,
                 size = size,
                 topLeft = topLeft,
-                useCenter = true,
+                useCenter = false,
                 alpha = alpha
             )
             startAngle += percentage
         }
     }
 }
+
+private enum class AnimatedCircleProgress { START, END }
 
 @Preview(showBackground = true)
 @Composable
