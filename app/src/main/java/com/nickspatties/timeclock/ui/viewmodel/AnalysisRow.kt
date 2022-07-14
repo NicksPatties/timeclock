d1bc113703b7e62c896d19de07fd67a836f2c1aa
NicksPatties <nickspatties@gmail.com>
2022-07-14 17:19:13 -0600

    Remove example test. Fix and test getPercentage function.

--- app/src/main/java/com/nickspatties/timeclock/ui/viewmodel/AnalysisRow.kt
+++ app/src/main/java/com/nickspatties/timeclock/ui/viewmodel/AnalysisRow.kt
@@ -4,7 +4,16 @@ import com.nickspatties.timeclock.util.generateColorFromString
 
 class AnalysisRow(val name: String, val millis: Long, val id: Long) {
     val color = generateColorFromString(name)
+
+    /**
+     * Gets the percentage of time that an AnalysisRow represents with a given
+     * amount of total milliseconds.
+     *
+     * @param totalMillis the total number of milliseconds that this row's millis value divides into
+     * @return a percentage, or 0f if totalMillis is less than or equal to 0
+     */
     fun getPercentage(totalMillis: Long): Float {
-        return millis / totalMillis.toFloat()
+        if(totalMillis <= 0L) return 0f
+        return millis / totalMillis.toFloat() * 100f
     }
 }
