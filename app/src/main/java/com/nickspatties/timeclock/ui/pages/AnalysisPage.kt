32da806b5fb7daf12fecf5e8493ff6165028d49d
NicksPatties <nickspatties@gmail.com>
2022-08-24 10:52:50 -0600

    Create AnalysisPageViewModel to make testing the analysis page easier.

--- app/src/main/java/com/nickspatties/timeclock/ui/pages/AnalysisPage.kt
+++ app/src/main/java/com/nickspatties/timeclock/ui/pages/AnalysisPage.kt
@@ -6,29 +6,29 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nickspatties.timeclock.R
 import com.nickspatties.timeclock.ui.components.*
-import com.nickspatties.timeclock.ui.viewmodel.AnalysisRow
+import com.nickspatties.timeclock.ui.viewmodel.AnalysisPageViewModelState
+import com.nickspatties.timeclock.util.decorateMillisWithDecimalHours
 import com.nickspatties.timeclock.util.generateColorFromString
 
 @Composable
 fun AnalysisPage(
-    currentSelectionString: String = "All time",
-    onSelectionStartButtonClick: () -> Unit = {},
-    onSelectionEndButtonClick: () -> Unit = {},
-    selectionStartButtonVisible: Boolean = false,
-    selectionEndButtonVisible: Boolean = true,
-    analysisPageRows: List<AnalysisRow>?,
-    totalSelectedHours: String = "",
-    openId: Long = -1,
-    changeRowId: (Long) -> Unit = {}
+    viewModelState: AnalysisPageViewModelState
 ) {
+    val currentSelectionString = viewModelState.currAnalysisPane.rangeName
+    val analysisPageRows = viewModelState.currAnalysisPane.rowData.observeAsState().value
+    val totalSelectedHours =
+        decorateMillisWithDecimalHours(viewModelState.currAnalysisPane.selectedMillis)
+    val openId = viewModelState.currAnalysisPane.selectedAnalysisRowId
+    val changeRowId = viewModelState.currAnalysisPane::changeSelectedAnalysisRowId
+
     Scaffold {
         Column(
             modifier = Modifier.padding(it)
@@ -36,10 +36,10 @@ fun AnalysisPage(
             TimeRangeSelector(
                 modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
                 centerText = currentSelectionString,
-                startButtonFunction = onSelectionStartButtonClick,
-                startButtonVisible = selectionStartButtonVisible,
-                endButtonFunction = onSelectionEndButtonClick,
-                endButtonVisible = selectionEndButtonVisible
+                startButtonFunction = viewModelState::onDateRangeStartButtonClick,
+                startButtonVisible = viewModelState.isDateRangeStartButtonVisible(),
+                endButtonFunction = viewModelState::onDateRangeEndButtonClick,
+                endButtonVisible = viewModelState.isDateRangeEndButtonVisible()
             )
             if (analysisPageRows != null && analysisPageRows.isNotEmpty()) {
                 var totalMillis = 0L
@@ -125,53 +125,4 @@ fun AnalysisPage(
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AnalysisPagePreview() {
-    val pairsList = mutableListOf<AnalysisRow>()
-    var items = 50
-    var id = 0L
-    while (items > 0) {
-        pairsList.add(
-            AnalysisRow("Item $items", items * 100000L, id)
-        )
-        id++
-        items--
-    }
-    AnalysisPage(analysisPageRows = pairsList)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AnalysisPagePairsAreNull() {
-    AnalysisPage(analysisPageRows = null)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AnalysisPageNoPairs() {
-    AnalysisPage(analysisPageRows = listOf())
-}
-
-/**
- * Demonstrates that the AnalysisPage component can handle an AnalysisRow with
- * no milliseconds of duration instead of printing "NaN%"
- *
- * @see <a href="https://github.com/NicksPatties/timeclock/issues/11">issue #11</a>
- * for more details
- */
-@Preview(showBackground = true)
-@Composable
-fun AnalysisPageTestNaNBug() {
-    AnalysisPage(
-        analysisPageRows = listOf(
-            AnalysisRow(
-                "item",
-                0,
-                0
-            )
-        )
-    )
 }
--- app/src/main/java/com/nickspatties/timeclock/ui/viewmodel/AnalysisPageViewModel.kt
+++ app/src/main/java/com/nickspatties/timeclock/ui/viewmodel/AnalysisPageViewModel.kt
@@ -1,57 +1,65 @@
 package com.nickspatties.timeclock.ui.viewmodel
 
 import android.app.Application
+import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import com.nickspatties.timeclock.R
 import com.nickspatties.timeclock.data.TimeClockEvent
 import com.nickspatties.timeclock.util.sortByNamesAndTotalMillis
 
 class AnalysisPageViewModel (
     timeClockEvents: LiveData<List<TimeClockEvent>>,
-    application: Application
-): AndroidViewModel(application) {
+    context: Context
+): ViewModel() {
     /**
      * Analysis page properties
      */
     private val allTimeAnalysisPane = AnalysisPane(
         eventData = timeClockEvents,
         rowTransformation = ::sortByNamesAndTotalMillis,
-        rangeName = getApplication<Application>().applicationContext
+        rangeName = context
             .getString(R.string.analysis_page_all_time)
     )
     private val todayAnalysisPane = AnalysisPane(
         eventData = timeClockEvents,
         rowTransformation = ::sortByNamesAndTotalMillis,
-        rangeName = getApplication<Application>().applicationContext
+        rangeName = context
             .getString(R.string.analysis_page_today),
         daysInRange = 1
     )
     private val lastWeekAnalysisPane = AnalysisPane(
         eventData = timeClockEvents,
         rowTransformation = ::sortByNamesAndTotalMillis,
-        rangeName = getApplication<Application>().applicationContext
-            .getString(R.string.analysis_page_last_week),
+        rangeName = context.getString(R.string.analysis_page_last_week),
         daysInRange = 7
     )
     private val lastMonthAnalysisPane = AnalysisPane(
         eventData = timeClockEvents,
         rowTransformation = ::sortByNamesAndTotalMillis,
-        rangeName = getApplication<Application>().applicationContext
-            .getString(R.string.analysis_page_last_month),
+        rangeName = context.getString(R.string.analysis_page_last_month),
         daysInRange = 30
     )
-    private var analysisPanes = listOf(
-        allTimeAnalysisPane,
-        todayAnalysisPane,
-        lastWeekAnalysisPane,
-        lastMonthAnalysisPane
+
+    val state = AnalysisPageViewModelState(
+        panes = listOf(
+            allTimeAnalysisPane,
+            todayAnalysisPane,
+            lastWeekAnalysisPane,
+            lastMonthAnalysisPane
+        )
     )
+}
+
+class AnalysisPageViewModelState(
+    private val panes: List<AnalysisPane> = listOf()
+) {
     private var currDateRangeIndex = 0
-    var currAnalysisPane by mutableStateOf(analysisPanes[currDateRangeIndex])
+    var currAnalysisPane by mutableStateOf(panes[currDateRangeIndex])
 
     fun onDateRangeStartButtonClick() {
         if (currDateRangeIndex > 0) {
@@ -64,19 +72,19 @@ class AnalysisPageViewModel (
     }
 
     fun onDateRangeEndButtonClick() {
-        if (currDateRangeIndex < analysisPanes.size - 1) {
+        if (currDateRangeIndex < panes.size - 1) {
             updateAnalysisPane(currDateRangeIndex + 1)
         }
     }
 
     fun isDateRangeEndButtonVisible(): Boolean {
-        return currDateRangeIndex < analysisPanes.size - 1
+        return currDateRangeIndex < panes.size - 1
     }
 
     fun updateAnalysisPane(index: Int) {
-        analysisPanes[currDateRangeIndex].resetSelectedRowAndMillis()
+        panes[currDateRangeIndex].resetSelectedRowAndMillis()
         // change the current index
         currDateRangeIndex = index
-        currAnalysisPane = analysisPanes[currDateRangeIndex]
+        currAnalysisPane = panes[currDateRangeIndex]
     }
 }
